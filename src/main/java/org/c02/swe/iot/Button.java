2f907525ae222a7582e3ac0078b88849eb67daee
Hade Mohamed <hade.mohamed@outlook.de>
2021-11-20 10:39:51 +0100

    added LedButtonApiDemo3

--- src/main/java/org/c02/swe/iot/Button.java
+++ src/main/java/org/c02/swe/iot/Button.java
@@ -7,11 +7,6 @@ import org.c02.swe.iot.cloud.api.IParticleApi;
 import org.c02.swe.iot.cloud.api.ParticleException;
 
 public class Button implements IButton {
-
-
-    private int position;
-    private Color color;
-
     private IParticleApi api;
 
     public Button(IParticleApi wrapperInstance) {
@@ -20,8 +15,8 @@ public class Button implements IButton {
 
 
     public void setLed(int position, Color color) throws ParticleException {
-
-        String parameter = color.getRed() + "" + color.getGreen() + "" + color.getBlue();
+        String parameter = formatColor(color.getRed()) + formatColor(color.getGreen())
+                + formatColor(color.getBlue());
         int length = String.valueOf(position).length();
 
         if (length == 1){
@@ -31,18 +26,48 @@ public class Button implements IButton {
         }
 
         api.callMethod("led", parameter);
-
     }
 
     public void allLedsOff() throws ParticleException {
         api.callMethod("ledsOff", null);
     }
 
-    public void setLed(LedStatus status) {
+    public void setLed(LedStatus status) throws ParticleException {
+        String pos = getPosition(status.getPosition());
+        String r = this.formatColor(status.getColor().getRed());
+        String g = this.formatColor(status.getColor().getGreen());
+        String b = this.formatColor(status.getColor().getBlue());
+        String parameter = String.format("%s%s%s%s", pos,
+                r, g, b);
 
+        api.callMethod("led", parameter);
     }
 
-    public void setLeds(List<LedStatus> statuses) {
+    public void setLeds(List<LedStatus> statuses) throws ParticleException {
+        for (LedStatus status: statuses ) {
+            this.setLed(status);
+        }
+    }
 
+    private String formatColor(int color) {
+        int length = String.valueOf(color).length();
+
+        if(length == 1) return "00" + color;
+        if(length == 2) return "0" + color;
+
+        return String.valueOf(color);
+    }
+
+    private String getPosition(int pos) {
+        String parameter = "";
+        int length = String.valueOf(pos).length();
+
+        if (length == 1){
+            parameter = "0" + pos;
+        } else {
+            parameter = String.valueOf(pos);
+        }
+
+        return parameter;
     }
 }
