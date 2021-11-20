3fe691ae38df0a553f479e2930c9eccb8c3bd0b4
Thomas <thomas.gugatschka@gmail.com>
2021-11-20 10:16:12 +0100

    added method - setLed and allLedsOff

--- src/main/java/org/c02/swe/iot/Button.java
+++ src/main/java/org/c02/swe/iot/Button.java
@@ -9,28 +9,40 @@ import org.c02.swe.iot.cloud.api.ParticleException;
 public class Button implements IButton {
 
 
+    private int position;
+    private Color color;
+
+    private IParticleApi api;
 
     public Button(IParticleApi wrapperInstance) {
-
+        this.api = wrapperInstance;
     }
 
 
     public void setLed(int position, Color color) throws ParticleException {
 
+        String parameter = color.getRed() + "" + color.getGreen() + "" + color.getBlue();
+        int length = String.valueOf(position).length();
+
+        if (length == 1){
+            parameter = "0" + position + parameter;
+        } else {
+            parameter = position + parameter;
+        }
+
+        api.callMethod("led", parameter);
 
     }
 
     public void allLedsOff() throws ParticleException {
+        api.callMethod("ledsOff", null);
+    }
+
+    public void setLed(LedStatus status) {
 
     }
 
-    public void setLed(LedStatus status) throws ParticleException {
-
-
-    }
-
-    public void setLeds(List<LedStatus> statuses) throws ParticleException {
-
+    public void setLeds(List<LedStatus> statuses) {
 
     }
 }
